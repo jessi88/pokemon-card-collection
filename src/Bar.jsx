641bbe7c8611b1918bdcd260c64e2863e1f68dc2
Jessica Bosch <jessica.bosch88@gmail.com>
2026-03-23 14:22:49 +0100

    Designing the bars.

--- src/Bar.jsx
+++ src/Bar.jsx
@@ -1,18 +1,37 @@
-const Bar = ({ width, color, type } ) => {
-    return (
-        <div style={{ display: "flex", alignItems: "center", gap: 8, marginBottom: 6 }}>
-            <span>{type}
-            <div
-                style={{
-                    width: width,
-                    height: 10,
-                    backgroundColor: color,
-                    borderRadius: 4,
-                }}>
-            </div>
-            {width}</span>
-        </div>
-    );
-}
+const Bar = ({ width, color, type }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: 8,
+        marginBottom: 6,
+        width: "100%",
+      }}
+    >
+      <span style={{ width: 40 }}>{type}</span>
+
+      <div
+        style={{
+          flex: 1,
+          height: 10,
+          backgroundColor: "#eee",
+          borderRadius: 4,
+          overflow: "hidden",
+        }}
+      >
+        <div
+          style={{
+            width: `${width}%`,
+            height: "100%",
+            backgroundColor: color,
+          }}
+        />
+      </div>
+
+      <span style={{ width: 30, textAlign: "right" }}>{width}</span>
+    </div>
+  );
+};
 
 export default Bar;
